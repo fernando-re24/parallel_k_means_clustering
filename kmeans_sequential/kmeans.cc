0d5b37cf337e024e251c9e6133da7d7db74c1ad9
fernandore2024 <fernandorivasespinoza24@gmail.com>
2025-12-18 00:09:33 +0000

    initial commit

---
+++ kmeans_sequential/kmeans.cc
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+
+
+int find_cluster (double* kmeans, double* point, int k, int dim){}
+
+void calc_kmeans_next (double* data, int num_points, int dim, double* kmeans, double* kmeans_next, int k){}
+
+void calc_kmeans (double* data, int num_points, int dim, double* kmeans, int k, int m) {}
+
